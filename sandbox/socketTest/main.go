6de3821efc9cd19e8f420b565947da38f815c647
cybergavin <gavin.satur@moneris.com>
2020-11-12 19:34:27 -0500

    socketTest formatting

--- sandbox/socketTest/main.go
+++ sandbox/socketTest/main.go
@@ -1,10 +1,10 @@
 /*
-Author			:	cybergavin
-Creation Date	:	11th November 2020
-Description 	:	The socketTest program tests connectivity to a socket and returns "True" or "False" based on whether there is connectivity.
-					Input Parameters => (1) Protocol - tcp or udp
-										(2) Host String - <IP>:<port> or <FQDN>:<port>
-										(3) Timeout in seconds
+Author:	cybergavin
+Creation Date:	11th November 2020
+Description:	The socketTest program tests connectivity to a socket and returns "True" or "False" based on whether there is connectivity.
+				Input Parameters => (1) Protocol - tcp or udp
+									(2) Host String - <IP>:<port> or <FQDN>:<port>
+									(3) Timeout in seconds
 */
 package main
 
